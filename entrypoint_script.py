5039ce89984caed29329f111f8defcf093783e50
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2020-10-05 18:51:10 -0400

    now takes fmriprep outputs

--- entrypoint_script.py
+++ entrypoint_script.py
@@ -22,13 +22,15 @@ import decomp as dcp
 import utility_functions as uts
 #user inputs cl arguments separated by spaces
 parser = argparse.ArgumentParser(description='CHAP entrypoint script')
-parser.add_argument('--input_dir', type = str, help = 'qsirecon input directory')
-parser.add_argument('--surf_dir', type = str, help = 'please input BIDS-organized Freesurfer output dir here')
-parser.add_argument('--output_dir', type = str, help = 'Output directory')
-parser.add_argument('--analysis_level', type = str, help = 'participant or group')
+parser.add_argument('qsi_dir', type = str, help = 'qsirecon input directory')
+parser.add_argument('surf_dir', type = str, help = 'please input BIDS-organized Freesurfer output dir here')
+parser.add_argument('output_dir', type = str, help = 'Output directory')
+parser.add_argument('analysis_level', type = str, help = 'participant or group')
 parser.add_argument('--participant_label', type = str, help = 'Participant label(s) (not including sub-). If this parameter is not provided all subjects will be analyzed. Multiple participants can be specified with a space separated list')
-parser.add_argument('-p','--parc',help="path to parcellation file as vtk with %s for hem")
-parser.add_argument('-n', '--number', help='number of evecs to compute')
+parser.add_argument('-f', '--fprep_dir', type = str, help = 'please input BIDS-organized fMRIprep output dir here. Functional images should be in T1 (native) space')
+parser.add_argument('-t', '--tasks', type = str, help = 'fMRI tasks to be included in harmonic decomposition. Multiple tasks can be specified with a space separated list')
+parser.add_argument('-p','--parc',help= "path to parcellation file as vtk with %s for hem")
+parser.add_argument('-n', '--number', help= 'number of evecs to compute')
 args = parser.parse_args() 
 
 #set empty dict and list
@@ -38,41 +40,48 @@ if not os.path.exists(f'{args.output_dir}/chap'):
     os.mkdir(f'{args.output_dir}/chap') #create output directory
 
 #populate dicts with tck output files of reconstruction for each session, and {hem}.white
-#user input subjects
-if args.participant_label:
+if args.participant_label: #user input subjects
     subs = args.participant_label.split(" ")
-#for all subjects
-else:
-    subject_dirs = glob(os.path.join(args.input_dir, "sub-*"))
-    subs = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]                 
+else: #for all subjects
+    subject_dirs = glob(os.path.join(args.qsi_dir, "sub-*"))
+    subs = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]               
 for sub in subs:
     user_info[f'{sub}_info'] = {}  #create dict in user_info for each subjs info
     os.mkdir(f'{args.output_dir}/chap/sub-{sub}') #create output subject folder
     #surface files locations
     for hem in ['rh','lh']:
             user_info[f'{sub}_info'][f'{hem}_surf'] = []
-            user_info[f'{sub}_info'][f'{hem}_surf'].append(f'{args.surf_dir}/sub-{sub}/surf/{hem}.white')
-    #streamlines file locations
-    user_info[f'{sub}_info']['streamlines'] = []
-    if 'ses' in os.listdir(f'{args.input_dir}/sub-{sub}')[0]: #if multiple sessions
-        for ses in os.listdir(f'{args.input_dir}/sub-{sub}'): 
+            user_info[f'{sub}_info'][f'{hem}_surf'].append(f'{args.surf_dir}/sub-{sub}/surf/{hem}.white') 
+    user_info[f'{sub}_info']['streamlines'] = [] #streamlines file locations
+    if args.fprep_dir:
+        user_info[f'{sub}_info']['func'] = [] #functional file locations
+    if 'ses' in os.listdir(f'{args.qsi_dir}/sub-{sub}')[0]: #if multiple sessions
+        for ses in os.listdir(f'{args.qsi_dir}/sub-{sub}'): 
             if 'ses' in ses:
                 os.mkdir(f'{args.output_dir}/chap/sub-{sub}/{ses}') #create output session folders
-                for file in os.listdir(f'{args.input_dir}/sub-{sub}/{ses}-test/dwi'):
+                for file in os.listdir(f'{args.qsi_dir}/sub-{sub}/{ses}/dwi'):
                     if 'tck' in file:
                         user_info[f'{sub}_info']['streamlines'].append([ses, file]) #streamlines list with each session's .tck
+                if args.fprep_dir:
+                    for file in os.listdir(f'{args.fprep_dir}/sub-{sub}/{ses}/func'):
+                        for hem in ['L','R']:
+                            if f'space-fsnative_hemi-{hem}_bold.func.gii' in file:
+                                user_info[f'{sub}_info']['func'].append(file) #functional file locations               
     else: #if sub has just one session
         #os.mkdir(f'{args.output_dir}/chap/sub-{sub}/ses')
-        for file in os.listdir(f'{args.input_dir}/sub-{sub}/ses-test/dwi'):
+        for file in os.listdir(f'{args.qsi_dir}/sub-{sub}/dwi'):
             if 'tck' in file:
                 user_info[f'{sub}_info']['streamlines'].append(['ses', file])
-
-
+        if args.fprep_dir:
+            for file in os.listdir(f'{args.fprep_dir}/sub-{sub}/func'):
+                        for hem in ['L','R']:
+                            if f'space-fsnative_hemi-{hem}_bold.func.gii' in file:
+                                user_info[f'{sub}_info']['func'].append(file) #functional file locations
     for ses, file in user_info[f'{sub}_info']['streamlines']:
     #convert streamlines to .vtk using mrtrix
         tck_name = file.split('/')[-1][:-4]
         os.mkdir(f'{args.output_dir}/chap/sub-{sub}/{ses}/endpoints')
-        subprocess.check_call("./mrtrix_qsi_pipeline.sh %s %s %s" %(f'{args.input_dir}/sub-{sub}/{ses}-test/dwi', tck_name, f'{args.output_dir}/chap/sub-{sub}/{ses}/endpoints'), shell=True)
+        subprocess.check_call("./mrtrix_qsi_pipeline.sh %s %s %s" %(f'{args.qsi_dir}/sub-{sub}/{ses}-test/dwi', tck_name, f'{args.output_dir}/chap/sub-{sub}/{ses}/endpoints'), shell=True)
 	#construct surface coordinates, surface endpoints
         lh_surf_path =  user_info[f'{sub}_info']['lh_surf'][0]
         rh_surf_path = user_info[f'{sub}_info']['rh_surf'][0]
