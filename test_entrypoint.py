ca893534c3614cd653f01824b918f283b559e031
Brian Winston <61159065+bwinsto2@users.noreply.github.com>
2020-10-06 13:32:05 -0400

    Update test_entrypoint.py

--- test_entrypoint.py
+++ test_entrypoint.py
@@ -1,1 +1,6 @@
-print('yo whats up my man')
+import argparse
+parser = argparse.ArgumentParser(description='test CHAP entrypoint script')
+parser.add_argument('greeting', type = str, help = 'say hi')
+args = parser.parse_args() 
+if args.greeting == 'hi':
+  print('yo whats up my man')
