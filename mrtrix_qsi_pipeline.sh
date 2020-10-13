591cb8299329764b45230466ceae4447709d5d11
hptaylor <33815752+hptaylor@users.noreply.github.com>
2020-10-13 15:35:30 -0400

    Update mrtrix_qsi_pipeline.sh
    added take only endpoints

--- mrtrix_qsi_pipeline.sh
+++ mrtrix_qsi_pipeline.sh
@@ -2,4 +2,5 @@
 tck_path=$1
 tck_name=$2
 output_dir=$3
-tckconvert ${tck_path}/${tck_name}.tck ${output_dir}/${tck_name}_endpoints.vtk
+tckresample -endpoints ${tck_path}/${tck_name}.tck ${tck_path}/${tck_name}_endpoints.tck
+tckconvert ${tck_path}/${tck_name}_endpoints.tck ${output_dir}/${tck_name}_endpoints.vtk
