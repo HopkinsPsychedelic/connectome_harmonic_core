b006465d4609fa5ecd1ca423e271bccc61e250fd
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2020-11-02 16:41:32 -0500

    fix split thing and add freesurfer license file stuff

--- entrypoint_script.py
+++ entrypoint_script.py
@@ -13,6 +13,7 @@ test
 
 import argparse
 import os
+import shutil
 from glob import glob
 import numpy as np
 import subprocess
@@ -24,16 +25,19 @@ import utility_functions as uts
 import construct_harmonics as cs
 #user inputs cl arguments separated by spaces
 parser = argparse.ArgumentParser(description='Connectome Harmonic Analysis Pipeline (CHAP)')
-parser.add_argument('qsi_dir', type = str, help = 'qsirecon input directory')
-parser.add_argument('surf_dir', type = str, help = 'please input BIDS-organized Freesurfer output dir here')
-parser.add_argument('output_dir', type = str, help = 'Output directory')
-parser.add_argument('analysis_level', type = str, help = 'participant or group')
+parser.add_argument('qsi_dir', type = str, help = 'qsirecon output directory')
+parser.add_argument('surf_dir', type = str, help = 'BIDS-organized Freesurfer output directory')
+parser.add_argument('output_dir', type = str, help = 'CHAP output directory')
+parser.add_argument('analysis_level', type = str, help = 'Participant or group mode')
+parser.add_argument('fs_license_file', type = str, help = 'Path to Freesurfer license file (including filename)')
 parser.add_argument('--participant_label', type = str, help = 'Participant label(s) (not including sub-). If this parameter is not provided all subjects will be analyzed. Multiple participants can be specified with a space separated list')
-parser.add_argument('--fprep_dir', type = str, help = 'please input BIDS-organized fMRIprep output dir here. Functional images should be in fsnative space')
+parser.add_argument('--fprep_dir', type = str, help = 'BIDS-organized fMRIprep output dir. Functional images should be in T1w/anat space')
 parser.add_argument('--parc', type = str, help = "path to parcellation file as vtk with %s for hem")
 parser.add_argument('--number', type = str, help = 'number of evecs to compute')
 
-args = parser.parse_args()  
+args = parser.parse_args() 
+#place Freesurfer license file
+shutil.copyfile(args.fs_license_file, '/opt/freesurfer-6.0.0')
 #set empty dict and list
 user_info = {}
 subs = []
@@ -41,7 +45,7 @@ if not os.path.exists(f'{args.output_dir}/chap'):
     os.mkdir(f'{args.output_dir}/chap') #create output directory
 
 #populate dicts with tck output files of reconstruction for each session, and {hem}.white
-print('Loading subjects...')
+print('[CHAP] Loading subjects...')
 if args.participant_label: #user input subjects
     subs = args.participant_label.split(" ")
 else: #for all subjects
@@ -50,20 +54,20 @@ else: #for all subjects
 for sub in subs:
     user_info[f'{sub}_info'] = {}  #create dict in user_info for each subjs info
     os.mkdir(f'{args.output_dir}/chap/sub-{sub}') #create output subject folder
-    print(f'Reconstructing surfaces for {sub}...')
+    print(f'[CHAP] Reconstructing surfaces for {sub}...')
     os.system(f'bash /home/neuro/repo/surface_resample.sh {args.surf_dir}/sub-{sub}/surf /home/neuro/repo') #run surface reconstruction script
     user_info[f'{sub}_info']['streamlines'] = [] #streamlines file locations
     if args.fprep_dir:
         user_info[f'{sub}_info']['func'] = [] #functional file locations
     if any('ses' in x for x in os.listdir(f'{args.qsi_dir}/sub-{sub}')): #if multiple sessions
-        print(f'Detected multiple sessions for {sub}')
+        print(f'[CHAP] Detected multiple sessions for {sub}')
         for ses in os.listdir(f'{args.qsi_dir}/sub-{sub}'): 
             if 'ses' in ses:
-                print(f'Locating files for {ses}...')
+                print(f'[CHAP] Locating files for {ses}...')
                 os.mkdir(f'{args.output_dir}/chap/sub-{sub}/{ses}') #create output session folders
                 for file in os.listdir(f'{args.qsi_dir}/sub-{sub}/{ses}/dwi'):
                     if 'tck' in file:
-                        print('Located streamlines')
+                        print('[CHAP] Located streamlines')
                         user_info[f'{sub}_info']['streamlines'].append([ses, file]) #streamlines list with each session's .tck
                 if args.fprep_dir:
                     for file in os.listdir(f'{args.fprep_dir}/sub-{sub}/{ses}/func'):
@@ -71,7 +75,7 @@ for sub in subs:
                             if f'space-fsnative_hemi-{hem}_bold.func.gii' in file:
                                 user_info[f'{sub}_info']['func'].append([ses, file]) #functional file locations               
     else: #if sub has just one session
-        print('Detected only one session')
+        print('[CHAP] Detected only one session')
         for file in os.listdir(f'{args.qsi_dir}/sub-{sub}/dwi'):
             if 'tck' in file:
                 print('Located streamlines')
@@ -81,7 +85,7 @@ for sub in subs:
                         for hem in ['L','R']:
                             if f'space-fsnative_hemi-{hem}_bold.func.gii' in file: #CHANGE THIS
                                 user_info[f'{sub}_info']['func'].append(file) #functional file locations  
-    multises = any('ses' in x for x in user_info[f'{sub}_info']['streamlines']) #check whether multiple sessions, set output var
+    multises = any('ses' in x[0] for x in user_info[f'{sub}_info']['streamlines']) #check whether multiple sessions, set output var
     if multises:
         for ses, file in user_info[f'{sub}_info']['streamlines']:
             #convert streamlines to .vtk using mrtrix
