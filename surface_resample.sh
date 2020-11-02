469d6bc34d37fb68d249e35e80f76c25b748517c
Brian Winston <61159065+bwinsto2@users.noreply.github.com>
2020-11-02 18:47:56 -0500

    Update surface_resample.sh

--- surface_resample.sh
+++ surface_resample.sh
@@ -27,18 +27,18 @@ mris_convert ${subsurfdir}/rh.sphere.reg ${subsurfdir}/rh.sphere.reg.vtk
 
 
 
-./BuildSphericalInterpolationMapping ${subsurfdir}/lh.sphere.reg.vtk ${refspheredir}/lh.sphere.ico5.reg.vtk ${subsurfdir}/lh.mapping.vtk
+/home/neuro/repo/BuildSphericalInterpolationMapping ${subsurfdir}/lh.sphere.reg.vtk ${refspheredir}/lh.sphere.ico5.reg.vtk ${subsurfdir}/lh.mapping.vtk
 
-./BuildSphericalInterpolationMapping ${subsurfdir}/rh.sphere.reg.vtk ${refspheredir}/rh.sphere.ico5.reg.vtk ${subsurfdir}/rh.mapping.vtk
+/home/neuro/repo/BuildSphericalInterpolationMapping ${subsurfdir}/rh.sphere.reg.vtk ${refspheredir}/rh.sphere.ico5.reg.vtk ${subsurfdir}/rh.mapping.vtk
 
-./ShapeInterpolation ${subsurfdir}/lh.white.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.white.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.white.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.white.corresponded.vtk
 
-./ShapeInterpolation ${subsurfdir}/rh.white.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.white.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.white.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.white.corresponded.vtk
 
-./ShapeInterpolation ${subsurfdir}/lh.midthickness.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.midthickness.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.midthickness.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.midthickness.corresponded.vtk
 
-./ShapeInterpolation ${subsurfdir}/rh.midthickness.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.midthickness.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.midthickness.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.midthickness.corresponded.vtk
 
-./ShapeInterpolation ${subsurfdir}/lh.pial.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.pial.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.pial.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.pial.corresponded.vtk
 
-./ShapeInterpolation ${subsurfdir}/rh.pial.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.pial.corresponded.vtk
+/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.pial.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.pial.corresponded.vtk
