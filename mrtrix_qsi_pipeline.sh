9561010ec15b0e508a5177c7afa6f884a5a39ee5
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2020-11-03 14:36:49 -0500

    fixed issue w/ mrtrix saving inside qsiprep folder

--- mrtrix_qsi_pipeline.sh
+++ mrtrix_qsi_pipeline.sh
@@ -2,5 +2,5 @@
 tck_path=$1
 tck_name=$2
 output_dir=$3
-tckresample -endpoints ${tck_path}/${tck_name}.tck ${tck_path}/${tck_name}_endpoints.tck
-tckconvert ${tck_path}/${tck_name}_endpoints.tck ${output_dir}/${tck_name}.vtk
+tckresample -endpoints ${tck_path}/${tck_name}.tck ${output_dir}/${tck_name}_endpoints.tck
+tckconvert ${output_dir}/${tck_name}_endpoints.tck ${output_dir}/${tck_name}_endpoints.vtk
