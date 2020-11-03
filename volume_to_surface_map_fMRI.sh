aee4781223d6c54afdfa9be3a18dd204ddc9d36a
Patrick Taylor <hptaylor@live.unc.edu>
2020-11-03 16:19:41 -0500

    added volume_to_surface_map_fMRI.sh

--- volume_to_surface_map_fMRI.sh
+++ volume_to_surface_map_fMRI.sh
@@ -1,44 +1,27 @@
 #!/bin/bash
 
+
 subsurfdir=$1
 
-refspheredir=$2
+funcvolpath=$2
+
+lhsavepath=$3
+
+rhsavepath=$4
 
 
 
 
 
 
-mris_convert ${subsurfdir}/lh.midthickness ${subsurfdir}/lh.midthickness.vtk
-
-mris_convert ${subsurfdir}/rh.midthickness ${subsurfdir}/rh.midthickness.vtk
-
-mris_convert ${subsurfdir}/lh.white ${subsurfdir}/lh.white.vtk
-
-mris_convert ${subsurfdir}/rh.white ${subsurfdir}/rh.white.vtk
-
-mris_convert ${subsurfdir}/lh.pial ${subsurfdir}/lh.pial.vtk
-
-mris_convert ${subsurfdir}/rh.pial ${subsurfdir}/rh.pial.vtk
-
-mris_convert ${subsurfdir}/lh.sphere.reg ${subsurfdir}/lh.sphere.reg.vtk
-
-mris_convert ${subsurfdir}/rh.sphere.reg ${subsurfdir}/rh.sphere.reg.vtk
 
 
 
-/home/neuro/repo/BuildSphericalInterpolationMapping ${subsurfdir}/lh.sphere.reg.vtk ${refspheredir}/lh.sphere.ico5.reg.vtk ${subsurfdir}/lh.mapping.vtk
 
-/home/neuro/repo/BuildSphericalInterpolationMapping ${subsurfdir}/rh.sphere.reg.vtk ${refspheredir}/rh.sphere.ico5.reg.vtk ${subsurfdir}/rh.mapping.vtk
+/home/neuro/repo/workbench-2/bin_rh_linux64/wb_command -volume-to-surface-mapping ${funcvolpath} ${subsurfdir}/lh.midthickness.corresponded.gii ${lhsavepath} -ribbon-constrained ${subsurfdir}/lh.white.corresponded.gii ${subsurfdir}/lh.pial.corresponded.gii
 
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.white.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.white.corresponded.vtk
 
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.white.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.white.corresponded.vtk
 
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.midthickness.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.midthickness.corresponded.vtk
+/home/neuro/repo/workbench-2/bin_rh_linux64/wb_command -volume-to-surface-mapping ${funcvolpath} ${subsurfdir}/rh.midthickness.corresponded.gii ${rhsavepath} -ribbon-constrained ${subsurfdir}/rh.white.corresponded.gii ${subsurfdir}/rh.pial.corresponded.gii
 
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.midthickness.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.midthickness.corresponded.vtk
 
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/lh.pial.vtk ${subsurfdir}/lh.mapping.vtk ${subsurfdir}/lh.pial.corresponded.vtk
-
-/home/neuro/repo/ShapeInterpolation ${subsurfdir}/rh.pial.vtk ${subsurfdir}/rh.mapping.vtk ${subsurfdir}/rh.pial.corresponded.vtk
