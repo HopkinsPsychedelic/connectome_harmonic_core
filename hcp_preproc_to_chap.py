e1d1642763b2dce228de65c24ff3dc9f97d5361b
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2020-11-30 21:32:30 -0500

    changed ses

--- hcp_preproc_to_chap.py
+++ hcp_preproc_to_chap.py
@@ -16,32 +16,32 @@ import construct_harmonics as ch
 def hcp_chapper(args, sub, user_info):
     print(f'[CHAP] Creating directories for HCP subject {sub}')
     inout.if_not_exist_make(f'{args.output_dir}/hcp_preproc/sub-{sub}')
-    for ses in ['test', 'retest']:
+    for ses in ['ses-test', 'ses-retest']:
         user_info[f'{sub}_info'][ses], user_info[f'{sub}_info'][ses]['surfs'] = {}, {} 
-        inout.if_not_exist_make(f'{args.output_dir}/hcp_preproc/sub-{sub}/ses-{ses}')
-        inout.if_not_exist_make(f'{args.output_dir}/chap/sub-{sub}/ses-{ses}')
+        inout.if_not_exist_make(f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}')
+        inout.if_not_exist_make(f'{args.output_dir}/chap/sub-{sub}/{ses}')
         if args.fprep_dir:
-            inout.if_not_exist_make(f'{args.output_dir}/hcp_preproc/{sub}/ses-{ses}/func')         
-        for zipdir in os.listdir(f'{args.hcp_dir}/ses-{ses}'):
+            inout.if_not_exist_make(f'{args.output_dir}/hcp_preproc/{sub}/{ses}/func')         
+        for zipdir in os.listdir(f'{args.hcp_dir}/{ses}'):
             if sub in zipdir and 'md5' not in zipdir:
                 for bids_type in ['Structural', 'Diffusion']:
                     if bids_type in zipdir:
-                        with ZipFile(f'{args.hcp_dir}/ses-{ses}/{zipdir}', 'r') as zipObj:
+                        with ZipFile(f'{args.hcp_dir}/{ses}/{zipdir}', 'r') as zipObj:
                             print(f'[CHAP] Unzipping {sub} {ses} session {bids_type} directory')
-                            zipObj.extractall(f'{args.output_dir}/hcp_preproc/sub-{sub}/ses-{ses}/{bids_type}')       
-        diffusion_dir = f'{args.output_dir}/hcp_preproc/sub-{sub}/ses-{ses}/Diffusion/{sub}/T1w/Diffusion' 
-        struc_dir = f'{args.output_dir}/hcp_preproc/sub-{sub}/ses-{ses}/Structural/{sub}/T1w'
+                            zipObj.extractall(f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}/{bids_type}')       
+        diffusion_dir = f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}/Diffusion/{sub}/T1w/Diffusion' 
+        struc_dir = f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}/Structural/{sub}/T1w'
         user_info[f'{sub}_info'][ses]['surfs']['lh'] = f'{struc_dir}/fsaverage_LR32k/{sub}.L.white.32k_fs_LR.surf.gii'
         user_info[f'{sub}_info'][ses]['surfs']['rh'] = f'{struc_dir}/fsaverage_LR32k/{sub}.R.white.32k_fs_LR.surf.gii'
-        if os.path.exists(f'{args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix/10000000_endpoints.vtk'):
+        if os.path.exists(f'{args.output_dir}/chap/sub-{sub}/{ses}/mrtrix/10000000_endpoints.vtk'):
             print(f'[CHAP] Endpoints already detected')
-            user_info[f'{sub}_info'][ses]['endpoints'] = f'{args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix/10000000_endpoints.vtk'
+            user_info[f'{sub}_info'][ses]['endpoints'] = f'{args.output_dir}/chap/sub-{sub}/{ses}/mrtrix/10000000_endpoints.vtk'
         else: 
             print(f'[CHAP] Running MRtrix commands')
-            os.system(f'bash /home/neuro/repo/run_mrtrix_diffusion_pipeline.sh {diffusion_dir}/data.nii.gz {diffusion_dir}/bvals {diffusion_dir}/bvecs  {struc_dir}/T1w_acpc_dc_restore_brain.nii.gz {diffusion_dir}/nodif_brain_mask.nii.gz {args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix 10000000')
-            os.remove(f'{args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix/10000000.tck')
-            os.remove(f'{args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix/DWI.mif')
-            user_info[f'{sub}_info'][ses]['endpoints'] = f'{args.output_dir}/chap/sub-{sub}/ses-{ses}/mrtrix/10000000_endpoints.vtk'
+            os.system(f'bash /home/neuro/repo/run_mrtrix_diffusion_pipeline.sh {diffusion_dir}/data.nii.gz {diffusion_dir}/bvals {diffusion_dir}/bvecs  {struc_dir}/T1w_acpc_dc_restore_brain.nii.gz {diffusion_dir}/nodif_brain_mask.nii.gz {args.output_dir}/chap/sub-{sub}/{ses}/mrtrix 10000000')
+            os.remove(f'{args.output_dir}/chap/sub-{sub}/{ses}/mrtrix/10000000.tck')
+            os.remove(f'{args.output_dir}/chap/sub-{sub}/{ses}/mrtrix/DWI.mif')
+            user_info[f'{sub}_info'][ses]['endpoints'] = f'{args.output_dir}/chap/sub-{sub}/{ses}/mrtrix/10000000_endpoints.vtk'
         ch.construct_harmonics_calculate_spectra(args, sub, ses, user_info, multises = True)   
 
 
