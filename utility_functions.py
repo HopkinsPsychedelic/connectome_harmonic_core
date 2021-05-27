09d70d793642bee2c4df80866393a7deeedbea43
Brian Winston <bwinsto2@jh.edu>
2021-05-27 12:28:34 -0400

    icc

--- utility_functions.py
+++ utility_functions.py
@@ -11,7 +11,7 @@ import time
 import numpy as np
 import os 
 from scipy import sparse
-import icc
+#import icc
 
 def unmask_medial_wall(masked_feature,medial_wall_mask):
     unmasked_feature=np.zeros(len(medial_wall_mask))
@@ -212,9 +212,9 @@ def procrustes_alignment(data, reference=None, n_iter=10, tol=1e-5,
 
     return (aligned, reference) if return_reference else aligned
 
-  
+'''  
 def get_icc(measures):
     return icc.icc(measures,model='twoway',type='consistency',unit='single')
-            
+'''           
     
     
