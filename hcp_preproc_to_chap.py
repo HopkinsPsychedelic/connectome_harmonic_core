421be4edff42ac6a0ca43b6f57e028d6c9616983
Brian Winston <bwinsto2@jh.edu>
2021-06-20 16:48:30 -0400

    fuck

--- hcp_preproc_to_chap.py
+++ hcp_preproc_to_chap.py
@@ -58,7 +58,8 @@ def hcp_prep_for_ch(args, sub, u, multises, ses):
                     with ZipFile(f'{args.hcp_dir}/{ses}/{zipdir}', 'r') as zipObj:
                         print(f'[CHAP] Unzipping {sub} {ses} session {hcp_type} directory')
                         zipObj.extractall(f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}/{hcp_type}') #extract to intermediate
-    print('hello' + u[f'{sub}_info'][ses]['hcp_types'])
+    print('enzymes')
+    print(u[f'{sub}_info'][ses]['hcp_types'])
     for hcp_type in u[f'{sub}_info'][ses]['hcp_types']: 
         print(hcp_type)
         if not os.path.exists(f'{args.output_dir}/hcp_preproc/sub-{sub}/{ses}/{hcp_type}'): #if they don't have a task downloaded
