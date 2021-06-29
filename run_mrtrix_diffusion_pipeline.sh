151dff63a2321a918ca7956a0350f3cd2875a89b
Brian Winston <bwinsto2@jh.edu>
2021-06-29 14:52:30 -0400

    change mrtrix settings

--- run_mrtrix_diffusion_pipeline.sh
+++ run_mrtrix_diffusion_pipeline.sh
@@ -19,7 +19,7 @@ dwi2response msmt_5tt DWI.mif 5TT.mif RF_WM.txt RF_GM.txt RF_CSF.txt -voxels RF_
 
 dwi2fod msmt_csd DWI.mif RF_WM.txt WM_FODs.mif RF_GM.txt GM.mif RF_CSF.txt CSF.mif -mask ${brainmask}
 
-tckgen WM_FODs.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic WM_FODs.mif -maxlength 250 -select ${num_streamlines} -cutoff 0.06
+tckgen WM_FODs.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic WM_FODs.mif -maxlength 250 -minlength 30 -select ${num_streamlines} -cutoff 0.33
 
 tckresample -endpoints ${num_streamlines}.tck ${num_streamlines}_endpoints.tck
 
