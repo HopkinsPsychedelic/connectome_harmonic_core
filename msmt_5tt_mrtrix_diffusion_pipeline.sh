1241ddf71df9e6edb386f9e37d1afdb021172825
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2021-08-10 11:41:19 -0400

    mrtrix pipeline

--- msmt_5tt_mrtrix_diffusion_pipeline.sh
+++ msmt_5tt_mrtrix_diffusion_pipeline.sh
@@ -3,7 +3,7 @@
 dMRIdatapath=$1
 bvals=$2
 bvecs=$3
-T1path=$4
+freesurfer_out=$4
 brainmask=$5
 intermediary_output_path=$6
 num_streamlines=$7
@@ -11,7 +11,7 @@ num_streamlines=$7
 mkdir ${intermediary_output_path}
 cd ${intermediary_output_path}
 
-5ttgen fsl ${T1path} 5TT.mif -premasked
+5ttgen hsvs ${freesurfer_out} 5TT.mif 
 
 mrconvert ${dMRIdatapath} DWI.mif -fslgrad ${bvecs} ${bvals} -datatype float32 -stride 0,0,0,1
 
@@ -19,7 +19,7 @@ dwi2response msmt_5tt DWI.mif 5TT.mif RF_WM.txt RF_GM.txt RF_CSF.txt -voxels RF_
 
 dwi2fod msmt_csd DWI.mif RF_WM.txt WM_FODs.mif RF_GM.txt GM.mif RF_CSF.txt CSF.mif -mask ${brainmask}
 
-tckgen WM_FODs.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic WM_FODs.mif -maxlength 250 -select ${num_streamlines} -cutoff 0.06
+tckgen WM_FODs.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic WM_FODs.mif -maxlength 250 -select ${num_streamlines} -power 0.33
 
 tckresample -endpoints ${num_streamlines}.tck ${num_streamlines}_endpoints.tck
 
