a237b2c1659c2a61300675d428c94a544915b032
bwinsto2 <61159065+bwinsto2@users.noreply.github.com>
2021-08-17 10:28:38 -0400

    dhollander

--- dhollander_mrtrix_pipeline.sh
+++ dhollander_mrtrix_pipeline.sh
@@ -3,7 +3,7 @@
 dMRIdatapath=$1
 bvals=$2
 bvecs=$3
-T1path=$4
+freesurfer_out=$4
 brainmask=$5
 intermediary_output_path=$6
 num_streamlines=$7
@@ -11,27 +11,19 @@ num_streamlines=$7
 mkdir ${intermediary_output_path}
 cd ${intermediary_output_path}
 
-#3dresample -rmode NN -prefix resampled_mask.nii -input } 
-#AFNI resample anatomical
-
-5ttgen fsl ${T1path} 5TT.mif -premasked
+5ttgen hsvs ${freesurfer_out} 5TT.mif 
 
 mrconvert ${dMRIdatapath} DWI.mif -fslgrad ${bvecs} ${bvals} -datatype float32 -stride 0,0,0,1
 
-dwi2response dhollander DWI.mif wm_txt.txt gm_txt.txt csf_txt.txt -force
-#txt files are outputs
+mrconvert ${brainmask} brainmask.mif -datatype bit
 
-dwi2fod msmt_csd DWI.mif wm_txt.txt wm_fod.mif gm_txt.txt gm_fod.mif csf_txt.txt csf_fod.mif -lmax 4,8,8 -mask ${brainmask}
-#takes outputs of 
+dwi2response dhollander DWI.mif RF_WM.txt RF_GM.txt RF_CSF.txt -mask brainmask.mif
 
-mtnormalise wm_fod.mif wm_fod_norm.mif gm_fod.mif gm_fod_norm.mif csf_fod.mif csf_fod_norm.mif -mask ${brainmask}
-#qsiprep step
+dwi2fod msmt_csd DWI.mif RF_WM.txt WM_FODs.mif RF_GM.txt GM.mif RF_CSF.txt CSF.mif -lmax 10,0,0  
 
-tckgen wm_fod.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic wm_fod.mif -maxlength 250 -minlength 30 -select ${num_streamlines} -power 0.33
-#input and output are first two arguments
+mrconvert WM_FODs.mif - -coord 3 0 | mrcat FOD_CSF.mif FOD_GM.mif - tissues.mif -axis 3
 
-#tcksift2 ${num_streamlines}.tck wm_fod.mif -act 5TT.mif -out_mu out_mu.txt -out_weights sift_weights.txt
-#are all the other options part of default qsi?
+tckgen WM_FODs.mif ${num_streamlines}.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic WM_FODs.mif -maxlength 250 -select ${num_streamlines} -power 0.33
 
 tckresample -endpoints ${num_streamlines}.tck ${num_streamlines}_endpoints.tck
 
